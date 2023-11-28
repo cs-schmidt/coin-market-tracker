22c4fd4692c36993526f9415d8916271c8a9f389
cs-schmidt <ebs.euler@gmail.com>
2023-11-27 19:05:12 -0600

    feat: initialize app
    Setup a minimal Firebase app using React, TypeScript, and Bootstrap. The
    supporting build system was also established using Webpack, Babel, and
    PostCSS.

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <h1>Coin Market Tracker</h1>
+  );
+}
---
+++ src/services/firebase.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCcM_8_SxuISxsIufrOcPKsUo_XfJqICnE",
+  authDomain: "coin-market-tracker-ba34d.firebaseapp.com",
+  projectId: "coin-market-tracker-ba34d",
+  storageBucket: "coin-market-tracker-ba34d.appspot.com",
+  messagingSenderId: "596122549205",
+  appId: "1:596122549205:web:1a213068a283658ce19b5c"
+});
