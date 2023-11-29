7e681964fdbe1f0609a4c5521ae5951a14486318
cs-schmidt <ebs.euler@gmail.com>
2023-11-29 16:46:20 -0600

    ci: automate codebase linting and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
-  return (
-    <h1>Coin Market Tracker</h1>
-  );
+  return <h1>Coin Market Tracker</h1>;
 }
